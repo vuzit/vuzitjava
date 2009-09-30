33362e6d0f86e5a6bae298f420872bc4cccc4d60
bmatzelle <bmatzelle@gmail.com>
2009-09-30 18:01:19 -0400

    The test environment through the VuzitCL command-line application has been setup with the jargs project code.  The load code can now be added and tested.  Some build and documentation fixes were added as well.
    Modified   README
    * Fixed a slew of documentation bugs that were caused by copying directly from the Vuzit.Net library.
    
    Modified   build.xml
    * Now the "bin" option creates the binary VuzitCL application along with the Vuzit.jar library.
    * Documented all of the Ant targets.
    
    Added      src/CmdLineParser.java
    Added the very cool jargs library for command-line parsing: http://github.com/purcell/jargs
    
    Modified   src/VuzitCL.java
    * printUsage: New method that prints the usage parameters of the application.
    * main: Added support for jargs such that it now takes the "keys" and "load" parameter so I can actually get the code working.
    
    Modified   src/com/vuzit/Document.java
    * Added all of the private variables with all of the data we use.
    * Added all of the getter methods so that the VuzitCL project compiles.

--- src/VuzitCL.java
+++ src/VuzitCL.java
@@ -1,6 +1,9 @@
 
 import com.vuzit.*;
 import java.util.Date;
+import java.util.Enumeration;
+import java.util.Vector;
+import jargs.gnu.CmdLineParser;
 
 public class VuzitCL
 {
@@ -9,14 +12,83 @@ public class VuzitCL
    */
   public static void main(String[] args)
   {
-    // TODO: Get parameters from command-line
-    com.vuzit.Service.setPublicKey("PUBLIC_KEY");
-    com.vuzit.Service.setPrivateKey("PRIVATE_KEY");
+    if(args.length < 1) {
+      printUsage();
+      System.exit(0);
+    }
 
+    CmdLineParser parser = new CmdLineParser();
+    CmdLineParser.Option keys = parser.addStringOption('k', "keys");
+    CmdLineParser.Option help = parser.addBooleanOption('h', "help");
+    CmdLineParser.Option load = parser.addStringOption('l', "load");
+
+    try {
+      parser.parse(args);
+    }
+    catch (CmdLineParser.OptionException e) {
+      System.err.println(e.getMessage());
+      printUsage();
+      System.exit(2);
+    }
+
+    // help command
+    Boolean helpValue = (Boolean)parser.getOptionValue(help);
+    if(helpValue != null && helpValue == true) {
+      printUsage();
+      System.exit(0);
+    }
+
+    // keys command
+    String keyValues = (String)parser.getOptionValue(keys);
+    if(keyValues == null) {
+      System.err.println("The --keys flag is a required flag");
+      printUsage();
+      System.exit(2);
+    }
+    String[] keyList = keyValues.split(",");
+    com.vuzit.Service.setPublicKey(keyList[0]);
+    com.vuzit.Service.setPrivateKey(keyList[1]);
+
+    // load command
+    String loadValue = (String)parser.getOptionValue(load);
+    if(loadValue == null) {
+      com.vuzit.Document document = com.vuzit.Document.findById(loadValue);
+      System.out.println("LOADED: " + document.getId());
+      System.out.println("title: " + document.getTitle());
+      System.out.println("subject: " + document.getSubject());
+      System.out.println("pages: " + document.getPageCount());
+      System.out.println("width: " + document.getPageWidth());
+      System.out.println("height: " + document.getPageHeight());
+      System.out.println("size: " + document.getFileSize());
+    }
+
+    // TODO: Delete this old junk code
     Date date = new Date(1247967923000L);
     System.out.println(date.toString());
 
     String sig = com.vuzit.Service.signature("show", "1bgi", date);
     System.out.println(sig);
+  }
+
+  /**
+   * Prints the usage information for the application.  
+   */
+  private static void printUsage()
+  {
+    System.out.println(
+      "VuzitCL - Vuzit Command Line\n" +
+      "Usage: vuzitcl -k [PUBLIC_KEY],[PRIVATE_KEY] [OPTIONS]\n" +
+      "\n" + 
+      "Options:\n" +
+      "   -k, --keys [PUB_KEY],[PRIV_KEY]  Developer API keys - REQUIRED\n" +
+      "   -u, --upload [PATH]              File to upload\n" +
+      "   -p, --public                     Make uploaded file public\n" +
+      "   -l, --load [ID]                  Loads the document data\n" +
+      "   -d, --delete [ID]                Deletes a document\n" +
+      "   -s, --service-url [URL]          Sets the service URL\n" +
+      "   -v, --verbose                    Prints more messages\n" +
+      "   -h, --help                       Show this message\n"
+      );
+  
   }
 }
--- src/com/vuzit/Document.java
+++ src/com/vuzit/Document.java
@@ -1,11 +1,83 @@
 
 package com.vuzit;
 
+/**
+ * Class for manipulating documents from Vuzit.  
+ */
 public class Document extends Base
 {
+  // Private class variables
+  private String id = null;
+  private String title = null;
+  private String subject = null;
+  private int pageCount = -1;
+  private int pageWidth = -1;
+  private int pageHeight = -1;
+  private int fileSize = -1;
+
+  // Public instance data members
+
+  /**
+   * Returns the web id of the document.  
+   */
+  public String getId() {
+    return id;
+  }
+
+  /**
+   * Returns the title of the document.  
+   */
+  public String getTitle() {
+    return title;
+  }
+
+  /**
+   * Returns the subject of the document.  
+   */
+  public String getSubject() {
+    return subject;
+  }
+
+  /**
+   * Returns the number of pages in a document.  
+   */
+  public int getPageCount() {
+    return pageCount;
+  }
+
+  /**
+   * Returns the page width of the document.  
+   */
+  public int getPageWidth() {
+    return pageWidth;
+  }
+
+  /**
+   * Returns the page height of the document.  
+   */
+  public int getPageHeight() {
+    return pageHeight;
+  }
+
+  /**
+   * Returns the file size of the document.  
+   */
+  public int getFileSize() {
+    return fileSize;
+  }
+
+  // Public static methods
+
+  /**
+   * Loads a document by the web id.  
+   */
   public static Document findById(String webId)
   {
     Document result = new Document();
+    result.id = "test";
+
+    // TODO: Add the loading code in here
+
     return result;
   }
 }
